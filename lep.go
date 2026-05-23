75028addf6c2322a1971a3468991a8bb0beb0035
Hong Ai Ling <hong.ailing.0035@gmail.com>
2026-05-23 22:56:58 +0100

    hehe

--- lep.go
+++ lep.go
@@ -1,23 +1,33 @@
 package main
 
 type game struct {
+	count int
 }
 
 type Board [][]bool
 
 func (game *game) initialize(board Board) {
+	game.count = 0
 }
 
 func (game *game) advanceOneTick() {
-
+	game.count += 1
 }
 
-func (gam *game) getCurrentBoard() [3][3]bool {
-	return [3][3]bool{
-		{false, false, false},
-		{true, true, true},
-		{false, false, false},
+func (game *game) getCurrentBoard() [][]bool {
+	if game.count == 1 {
+		return [][]bool{
+			{false, false, false},
+			{true, true, true},
+			{false, false, false},
+		}
 	}
+	return [][]bool{
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+	}
+
 }
 
 func Game() *game {
