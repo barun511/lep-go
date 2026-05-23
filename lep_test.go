af146e7e458141e2fcf1861139b70eb2b84fe8c8
Barun Parruck <barun.parruck@gmail.com>
2026-05-23 22:09:49 +0100

    Add board test

--- lep_test.go
+++ lep_test.go
@@ -4,9 +4,20 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
+func TestInitialGame(t *testing.T) {
 	game := Game()
 	if game == nil {
 		t.Errorf("Wah wah no game")
 	}
+}
+
+func TestCanInitializeGame(t *testing.T) {
+	game := Game()
+
+	board := [2][2]bool{
+		{true,false},
+		{false,true},
+	}
+
+	game.initialize(board)
 }
