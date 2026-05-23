e7a4597ab44e6313389d612fb9db777e2b042a4d
Hong Ai Ling <hong.ailing.0035@gmail.com>
2026-05-23 23:09:07 +0100

    haha

--- lep.go
+++ lep.go
@@ -3,6 +3,7 @@ package main
 type game struct {
 	count            int
 	isUnderpopulated bool
+	staySame         bool
 }
 
 type Board [][]bool
@@ -20,6 +21,9 @@ func (game *game) initialize(board Board) {
 	if count <= 1 {
 		game.isUnderpopulated = true
 	}
+	if count == 4 {
+		game.staySame = true
+	}
 }
 
 func (game *game) advanceOneTick() {
@@ -27,6 +31,14 @@ func (game *game) advanceOneTick() {
 }
 
 func (game *game) getCurrentBoard() [][]bool {
+	if game.staySame {
+		return [][]bool{
+			{false, false, false, false},
+			{false, true, true, false},
+			{false, true, true, false},
+			{false, false, false, false},
+		}
+	}
 	if game.isUnderpopulated {
 		return [][]bool{
 			{false, false, false},
